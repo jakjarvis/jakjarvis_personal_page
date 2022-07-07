9d9730ad92a0558925cd54d16c0f0097587fd679
Jono Jarvis <j.a.k.jarvis@gmail.com>
2022-07-07 10:55:59 +0200

    Added functionality to select a dice and add it to the selected dice area

--- resources/scripts.js
+++ resources/scripts.js
@@ -4,6 +4,11 @@
 // Only one player
 
 /* ---- VARIABLES ---- */
+let diceValues = [1, 2, 3, 4, 5, 6];
+let diceColor = ["white", "grey", "yellow", "blue", "green", "pink"];
+let lock = true;
+
+/* ---- ELEMENTS ---- */
 const rollBtn = document.querySelector(".roll_btn");
 const dicePosition = [
   document.querySelector(".die_0"),
@@ -13,48 +18,63 @@ const dicePosition = [
   document.querySelector(".die_4"),
   document.querySelector(".die_5"),
 ];
-let diceValues = [1, 2, 3, 4, 5, 6];
-let diceOrder = ["white", "grey", "yellow", "blue", "green", "pink"];
+const selectedDie = [
+  document.querySelector(".s_die1"),
+  document.querySelector(".s_die2"),
+  document.querySelector(".s_die3"),
+];
 
 /* ---- OBJECTS ---- */
-//const whiteDice = { color: "#fffff" };
-//const greyDice = { color: "#d3d3d3" };
-//const yellowDice = { color: "#ffbb00" };
-//const blueDice = { color: "#0000ff" };
-//const greenDice = { color: "#0da10d" };
-//const pinkDice = { color: "#ff8095" };
+let selectedDice = {
+  die1: null,
+  die2: null,
+  die3: null,
+};
+
+let dicePlate = {
+  die1: null,
+  die2: null,
+  die3: null,
+  die4: null,
+  die5: null,
+  die6: null,
+};
 
 /* ---- GAME FUNCTIONS ---- */
 // ROLL DICE
 rollBtn.addEventListener("click", function () {
   let diceOrder = orderDice();
-  console.log(diceOrder);
   displayDice(diceOrder);
   console.log(`Dice values are ${diceValues}, dice order is ${diceOrder}`);
+  lock = false;
 });
 
 function orderDice() {
   for (let i = 0; i < 6; i++) {
     let position = Math.trunc(Math.random() * 6);
-    let temp = diceOrder[position];
-    diceOrder[position] = diceOrder[i];
-    diceOrder[i] = temp;
+    let temp = diceColor[position];
+    diceColor[position] = diceColor[i];
+    diceColor[i] = temp;
   }
-  return diceOrder;
+  return diceColor;
 }
 
-function removeColor(diceOrder, i) {
+function removeColor(diceColor, i) {
   for (let j = 0; j < 6; j++) {
-    dicePosition[i].classList.remove(diceOrder[j]);
+    dicePosition[i].classList.remove(diceColor[j]);
   }
 }
 
-function displayDice(diceOrder) {
+function renderVisible() {
   for (let i = 0; i < 6; i++) {
-    console.log(`Die is ${dicePosition[i]}`);
-    removeColor(diceOrder, i);
-    dicePosition[i].classList.add(diceOrder[i]);
-    console.log(`Colour is ${diceOrder[i]}`);
+    dicePosition[i].classList.remove("hidden");
+  }
+}
+
+function displayDice(diceColor) {
+  for (let i = 0; i < 6; i++) {
+    removeColor(diceColor, i);
+    dicePosition[i].classList.add(diceColor[i]);
     let diceNumber = Math.trunc(Math.random() * 6) + 1;
     diceValues[i] = diceNumber;
     dicePosition[i].src = `resources/images/dice-${diceValues[i]}.png`;
@@ -64,10 +84,71 @@ function displayDice(diceOrder) {
     dicePosition[i].style.left = `${diceWidth}px`;
     let diceAngle = Math.trunc(Math.random() * 90);
     dicePosition[i].style.transform = `rotate(${diceAngle}deg)`;
+    renderVisible();
   }
 }
 
 // SELECT DICE
+dicePosition[0].addEventListener("click", function () {
+  selectDice(0);
+  lock = true;
+});
+dicePosition[1].addEventListener("click", function () {
+  selectDice(1);
+  lock = true;
+});
+dicePosition[2].addEventListener("click", function () {
+  selectDice(2);
+  lock = true;
+});
+dicePosition[3].addEventListener("click", function () {
+  selectDice(3);
+  lock = true;
+});
+dicePosition[4].addEventListener("click", function () {
+  selectDice(4);
+  lock = true;
+});
+dicePosition[5].addEventListener("click", function () {
+  selectDice(5);
+  lock = true;
+});
+
+function selectDice(position) {
+  if (lock == false) {
+    if (selectedDice.die1 == null) {
+      selectedDice.die1 = [diceValues[position], diceColor[position]];
+      console.log(
+        `First selected die is ${diceValues[position]} ${diceColor[position]}`
+      );
+      selectedDie[0].classList.add(diceColor[position]);
+      selectedDie[0].classList.remove("hidden");
+      selectedDie[0].src = `resources/images/dice-${diceValues[position]}.png`;
+    } else if (selectedDice.die2 == null) {
+      selectedDice.die2 = [diceValues[position], diceColor[position]];
+      console.log(
+        `Second selected die is ${diceValues[position]} ${diceColor[position]}`
+      );
+      selectedDie[1].classList.add(diceColor[position]);
+      selectedDie[1].classList.remove("hidden");
+      selectedDie[1].src = `resources/images/dice-${diceValues[position]}.png`;
+    } else if (selectedDice.die3 == null) {
+      selectedDice.die3 = [diceValues[position], diceColor[position]];
+      console.log(
+        `Third selected die is ${diceValues[position]} ${diceColor[position]}`
+      );
+      selectedDie[2].classList.add(diceColor[position]);
+      selectedDie[2].classList.remove("hidden");
+      selectedDie[2].src = `resources/images/dice-${diceValues[position]}.png`;
+    } else {
+      console.log(`All dice selected`);
+    }
+  }
+}
+
+function removeDice(selectedColor, selectedValue) {
+  //function to remove selected die from the pool and send lower dice to plate
+}
 // USE REROLL
 // USE RETURN
 // USE ADDITIONAL
