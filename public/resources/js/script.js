74957ed36b1f33e2b50c15dfd0ebf23a134eadaf
Jono Jarvis <j.a.k.jarvis@gmail.com>
2022-11-13 18:11:46 +0100

    comment out js from index.html

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,111 +1,160 @@
 $(document).ready(function () {
+  /* For the sticky navigation */
 
-    /* For the sticky navigation */
+  /* Scroll on button */
 
-    /* Scroll on button */
+  $(".js--scroll-to-start").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-about").offset().top },
+      1000
+    );
+  });
 
-    $('.js--scroll-to-start').click(function () {
-        $('html, body').animate({ scrollTop: $('.js--section-about').offset().top }, 1000);
+  /* navigation scroll*/
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
     });
+  });
 
-    /* navigation scroll*/
-    $(function () {
-        $('a[href*=#]:not([href=#])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                && location.hostname == this.hostname) {
+  $(".qualifications ul a").hover(function () {
+    $("#institute").removeClass().addClass($(this).attr("rel"));
+    $("#certificate").removeClass().addClass($(this).attr("rel"));
+    $("#description").removeClass().addClass($(this).attr("rel"));
+  });
 
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+  /* Sticky nav */
 
-    $(".qualifications ul a").hover(function () {
-        $("#institute").removeClass().addClass($(this).attr('rel'));
-        $("#certificate").removeClass().addClass($(this).attr('rel'));
-        $("#description").removeClass().addClass($(this).attr('rel'));
-    });
+  const sectionSplash = document.querySelector(".section-splash");
 
-    /* Sticky nav */
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+      console.log(ent);
+      if (ent.isIntersecting === false) {
+        document.querySelector(".header").classList.add("sticky");
+        sectionSplash.classList.add("sticky");
+      }
+      if (ent.isIntersecting === true) {
+        document.querySelector(".header").classList.remove("sticky");
+        sectionSplash.classList.remove("sticky");
+      }
+    },
+    {
+      // Looking inside the viewport
+      root: null,
+      // When 0% of the section is in the viewport
+      threshold: 0,
+      rootMargin: "-5px",
+    }
+  );
+  obs.observe(sectionSplash);
 
-    const sectionSplash = document.querySelector(".section-splash");
+  /* Mobile nav */
 
-    const obs = new IntersectionObserver(
-        function (entries) {
-            const ent = entries[0];
-            console.log(ent);
-            if (ent.isIntersecting === false) {
-                document.querySelector('.header').classList.add('sticky');
-                sectionSplash.classList.add('sticky');
-            }
-            if (ent.isIntersecting === true) {
-                document.querySelector('.header').classList.remove('sticky');
-                sectionSplash.classList.remove('sticky');
-            }
-        }, {
-        // Looking inside the viewport
-        root: null,
-        // When 0% of the section is in the viewport
-        threshold: 0,
-        rootMargin: "-5px"
-    });
-    obs.observe(sectionSplash);
+  const navBtn = document.querySelector(".mobile-nav-icon");
+  const mainNav = document.querySelector(".main-nav");
+  const openIcon = document.querySelector(".open-icon");
+  const closeIcon = document.querySelector(".close-icon");
+  const navList = document.querySelector(".nav-button");
 
-    /* Mobile nav */
+  navBtn.addEventListener("click", function () {
+    mainNav.classList.toggle("nav-open");
+    openIcon.classList.toggle("nav-open");
+    closeIcon.classList.toggle("nav-open");
+  });
 
-    const navBtn = document.querySelector('.mobile-nav-icon');
-    const mainNav = document.querySelector('.main-nav');
-    const openIcon = document.querySelector('.open-icon');
-    const closeIcon = document.querySelector('.close-icon');
+  /* Interests hovers */
 
-    navBtn.addEventListener('click', function () {
-        mainNav.classList.toggle('nav-open');
-        openIcon.classList.toggle('nav-open');
-        closeIcon.classList.toggle('nav-open');
-    })
+  $(".js--fly-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $(
+          "<span>I have enjoyed my pilots license since 2006 and flown hundreds of hours over three continents.</span>"
+        )
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 
-    /* Interests hovers */
+  $(".js--code-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $(
+          "<span>Check out my github for all my Python and html projects!</span>"
+        )
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 
-    $(".js--fly-icon").hover(function () {
-        $('.interest-text').append($("<span>I have enjoyed my pilots license since 2006 and flown hundreds of hours over three continents.</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
+  $(".js--stem-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $(
+          "<span>Interviewer and mentor for the Arkwright Scholarship Trust.</span>"
+        )
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 
-    $(".js--code-icon").hover(function () {
-        $('.interest-text').append($("<span>Check out my github for all my Python and html projects!</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
+  $(".js--tool-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $(
+          "<span>Owning a 1971 VW beetle and a Motorbike, you need to know how to fix everything!</span>"
+        )
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 
-    $(".js--stem-icon").hover(function () {
-        $('.interest-text').append($("<span>Interviewer and mentor for the Arkwright Scholarship Trust.</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
+  $(".js--music-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $("<span>Usually playing bass or piano in a Jazz band.</span>")
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 
-    $(".js--tool-icon").hover(function () {
-        $('.interest-text').append($("<span>Owning a 1971 VW beetle and a Motorbike, you need to know how to fix everything!</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
-
-    $(".js--music-icon").hover(function () {
-        $('.interest-text').append($("<span>Usually playing bass or piano in a Jazz band.</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
-
-    $(".js--hurl-icon").hover(function () {
-        $('.interest-text').append($("<span>Team and committee member for Berlin Setanta GAA.</span>"));
-    }, function () {
-        $('.interest-text').find("span").last().remove();
-    });
-
+  $(".js--hurl-icon").hover(
+    function () {
+      $(".interest-text").append(
+        $("<span>Team and committee member for Berlin Setanta GAA.</span>")
+      );
+    },
+    function () {
+      $(".interest-text").find("span").last().remove();
+    }
+  );
 });
