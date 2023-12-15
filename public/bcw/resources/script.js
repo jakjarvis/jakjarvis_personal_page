1eb7f1eceab25726e1cfc570bc4e61bd9337ccce
jakjarvis <j.a.k.jarvis@gmail.com>
2023-12-15 00:26:08 +0000

    [GHA] Upate BCW subfolder

--- public/bcw/resources/script.js
+++ public/bcw/resources/script.js
@@ -1,5 +1,33 @@
+/* PLACEHOLDER alert for portal */
 function loginAlert() {
   window.alert(
     "Coming soon! Login to browse your project documentation and pay your invoices."
   );
 }
+
+/* navigation scroll */
+var headerHeight = $("header").height();
+
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+      $target = $(target);
+    var scrollToPosition = $target.offset().top - headerHeight;
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: scrollToPosition,
+        },
+        1200,
+        "swing",
+        function () {
+          window.location.hash = target;
+          $("html").animate({ scrollTop: scrollToPosition }, 0);
+        }
+      );
+  });
+});
